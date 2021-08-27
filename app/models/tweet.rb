af4a0406cb9c4a15c4668f71e20049ec0d106736
devedux <devedux@gmail.com>
2021-08-27 15:32:13 +0000

    add validations for tweet model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,9 @@
 class Tweet < ApplicationRecord
+  # associations
   belongs_to :user
+  # has_many :comments, counter_cache: true
+
+  # validations
+  validates :body, presence: true
+  validates :body, length: { maximum: 140 }
 end
