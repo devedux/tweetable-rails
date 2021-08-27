d219ca6d05b44b3fc727409baf3c3620b8c7aa17
devedux <devedux@gmail.com>
2021-08-27 16:22:50 +0000

    add algoritm for create a tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,4 +3,18 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new
     @tweets = Tweet.all
   end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user = current_user
+    return render :index unless @tweet.save
+
+    redirect_to :root
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
+  end
 end
