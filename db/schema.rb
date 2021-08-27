76d8f48b9cd4bd3663d19ed5cf6b80b5191344ad
devedux <devedux@gmail.com>
2021-08-27 15:34:35 +0000

    generate migration for comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_27_152400) do
+ActiveRecord::Schema.define(version: 2021_08_27_153323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "tweet_id", null: false
+    t.text "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tweet_id"], name: "index_comments_on_tweet_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.text "body", null: false
@@ -34,5 +44,7 @@ ActiveRecord::Schema.define(version: 2021_08_27_152400) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "comments", "tweets"
+  add_foreign_key "comments", "users"
   add_foreign_key "tweets", "users"
 end
