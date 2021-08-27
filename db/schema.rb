64e69c58efa684c1bfe07aac01432248f2eaf562
devedux <devedux@gmail.com>
2021-08-27 15:27:05 +0000

    generate migration for tweets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_27_151240) do
+ActiveRecord::Schema.define(version: 2021_08_27_152400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tweets", force: :cascade do |t|
+    t.text "body", null: false
+    t.integer "comments_count", default: 0
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -25,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_08_27_151240) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "tweets", "users"
 end
