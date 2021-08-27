ffb81eadca9b8a5e02db1c2daed01145eff0c12f
devedux <devedux@gmail.com>
2021-08-27 21:23:02 +0000

    authenticate user actions skip index and show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: %i[index show]
+
+  def index; end
+
+  def show; end
 
   protected
 
