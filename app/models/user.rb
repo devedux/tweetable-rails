bbc4ba3ee0094b19e0f26a149ace7090aeab477b
devedux <devedux@gmail.com>
2021-08-27 15:51:40 +0000

    generate migration for devise in user table and add configuration for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,9 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :username, presence: true
   validates :username, uniqueness: true
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
