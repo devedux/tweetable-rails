0fc4bb8a30f6e493ed0bf1fa1510ec4259982438
devedux <devedux@gmail.com>
2021-08-27 15:23:27 +0000

    add validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  # constants
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+
+  # associations
+  # has_many :tweets, dependent: :destroy
+  # has_many :comments, dependent: :destroy
+
+  # validations
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: VALID_EMAIL_REGEX }, allow_blank: true
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :name, presence: true
 end
